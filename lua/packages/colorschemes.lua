85a67fdf292132d43e06e19b1ba127aaf81850ac
Samuel Jackson <dsiq3g@gmail.com>
2024-09-05 20:59:06 -0700

    Add tokyonight theme

--- lua/packages/colorschemes.lua
+++ lua/packages/colorschemes.lua
@@ -3,7 +3,7 @@ return {
 		'sainnhe/everforest',
 		lazy = false,
 		priority = 1000,
-		
+
 		-- Since this is a nice theme, set it right away to make sure
 		-- the system has a theme ASAP. We can then set further theme settings
 		-- in the colors config.
@@ -13,10 +13,17 @@ return {
 	},
 	{
 		'Mofiqul/dracula.nvim',
+		lazy = true,
 		config = true,
 	},
 	{
 		'ellisonleao/gruvbox.nvim',
+		lazy = true,
 		config = true,
 	},
+	{
+		'folke/tokyonight.nvim',
+		lazy = true,
+		config = true,
+	}
 }
