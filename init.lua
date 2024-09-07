b9b3749a0a4451f4651ba37c992a6bb6c79aafa6
Samuel Jackson <dsiq3g@gmail.com>
2024-09-06 23:36:42 -0700

    Add folding

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ require('keys')
 require('ui')
 require('scratch')
 require('files')
+require('folding')
 
 -- Comes last so all other general configuration can be completed and built upon in the plugins
 require('lazy_setup')
---
+++ lua/folding.lua
@@ -0,0 +1,17 @@
+local function enable_folding()
+	vim.opt.foldenable = true
+	vim.keymap.set('n', '<tab>', 'za', {desc = 'Fold/Unfold'})
+end
+
+vim.opt.foldmethod = 'indent' -- will have to check if there's a way to enable this with lsp
+vim.opt.foldnestmax = 100 -- prevent too many folds
+vim.opt.foldlevelstart = 20 -- start with pretty much everything open
+enable_folding()
+
+vim.keymap.set('n', '<leader>cf', function()
+	if vim.opt.foldenable:get() then
+		vim.opt.foldenable = false
+	else
+		enable_folding()
+	end
+end, {desc = 'Toggle folding'})
