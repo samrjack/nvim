d26772221f0a9bd5b2d2730e70f595b8213931fd
Samuel Jackson <dsiq3g@gmail.com>
2024-09-06 23:34:01 -0700

    Activate persistant undo

--- lua/files.lua
+++ lua/files.lua
@@ -3,3 +3,8 @@
 -- turn of backups, who needs em'?
 vim.opt.swapfile = false
 vim.opt.backup = false
+
+-- Undo info
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undodir"
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
--- lua/packages/undotree.lua
+++ lua/packages/undotree.lua
@@ -1,6 +1,9 @@
 return {
 	'mbbill/undotree',
 	lazy = true,
+	config = function()
+		vim.g.undotree_UndoDir = vim.opt.undodir:get()
+	end,
 	keys = {
 		{'<leader>u', vim.cmd.UndotreeToggle, desc = 'undo tree'},
 	},
