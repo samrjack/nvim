2b81ee3059ec0ef3c9cf54f57e65a4f0a5088a36
Samuel Jackson <dsiq3g@gmail.com>
2024-09-06 18:51:57 -0700

    Update UI option settings to be a bit better

--- lua/ui.lua
+++ lua/ui.lua
@@ -1,11 +1,20 @@
 -- Options that change the basic underlying vim UI settings
 
-vim.o.number = true -- Turn on line numbers
--- vim.o.relativenumber = true -- Enable relative line numbers
-vim.o.smartindent = true -- Automatically indent new lines
-vim.o.termguicolors = true -- Enable 24-bit RGB colors
+vim.opt.number = true -- Turn on line numbers
+vim.opt.smartindent = true -- Automatically indent new lines
+vim.opt.termguicolors = true -- Enable 24-bit RGB colors
 
 -- indentation
-vim.o.tabstop = 4 -- Number of spaces a tab represents
-vim.o.shiftwidth = 4 -- Number of visual spces per tab
-vim.o.softtabstop = 4 -- number of spaces in a tab when editing
+vim.opt.tabstop = 4 -- Number of spaces a tab represents
+vim.opt.shiftwidth = 4 -- Number of visual spces per tab
+vim.opt.softtabstop = 4 -- number of spaces in a tab when editing
+
+-- searching
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Scrolling with cursor
+vim.opt.scrolloff = 8
+
+-- refresh
+vim.opt.updatetime = 500 -- defaults to 4000
