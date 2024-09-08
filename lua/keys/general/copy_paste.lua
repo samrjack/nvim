35edc4adf4fbea510b599c822c4512bc8f5c1da7
Samuel Jackson <dsiq3g@gmail.com>
2024-09-07 22:01:28 -0700

    Fix command to copy to clipboard

--- lua/keys/general/copy_paste.lua
+++ lua/keys/general/copy_paste.lua
@@ -2,4 +2,4 @@
 vim.keymap.set('v', '<leader>p', '"_dP', {desc = 'paste and perserve register'})
 
 vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', {desc = 'Copy to system clipboard'})
-vim.keymap.set('n', '<leader>y', '"+Y', {desc = 'Copy line to system clipboard'})
+vim.keymap.set('n', '<leader>Y', '"+Y', {desc = 'Copy line to system clipboard'})
