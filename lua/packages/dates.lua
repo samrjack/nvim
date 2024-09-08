588fd53f9ba8a50645ba5c3f2325c4ff813e3a02
Samuel Jackson <dsiq3g@gmail.com>
2024-09-07 22:06:32 -0700

    Add package for better incrementing with dates

---
+++ lua/packages/dates.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		'https://github.com/tpope/vim-speeddating',
+		lazy = true,
+		keys = {
+			{'<C-a>', mode = {'n', 'v'}, desc = 'Increment number/date'},
+			{'<C-x>', mode = {'n', 'v'}, desc = 'Decrement number/date'},
+			{'d<C-a>', mode = {'n', 'v'}, desc = 'Set date UTC'},
+			{'d<C-x>', mode = {'n', 'v'}, desc = 'Set date local'},
+		},
+	},
+	{
+		'itchyny/calendar.vim'
+	}
+}
