6b60691bde096ee299ae16ff7f03b1897539e899
Samuel Jackson <dsiq3g@gmail.com>
2024-09-07 22:38:48 -0700

    Add a hex color colorizer

---
+++ lua/packages/colorizer.lua
@@ -0,0 +1,19 @@
+return {
+	'norcalli/nvim-colorizer.lua',
+	opts = {
+		'*',        -- Highlight all files, but customize some others.
+		css = {
+			RRGGBBAA = true, -- #RRGGBBAA hex codes
+			rgb_fn   = true, -- CSS rgb() and rgba() functions
+			hsl_fn   = true, -- CSS hsl() and hsla() functions
+			css      = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+			css_fn   = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+		},
+		html = {
+			RRGGBBAA = true, -- #RRGGBBAA hex codes
+		},
+		javascript = {
+			RRGGBBAA = true, -- #RRGGBBAA hex codes
+		}
+	},
+}
