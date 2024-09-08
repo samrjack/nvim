a6fba649b5c9b0eab733ebf228870611947ef58b
Samuel Jackson <dsiq3g@gmail.com>
2024-09-07 22:38:37 -0700

    Add vim-surround

---
+++ lua/packages/surround.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"kylechui/nvim-surround",
+		lazy = true,
+		opts = {},
+		keys = {
+			{ 'ys', desc = 'Add surrounding' },
+			{ 'cs', desc = 'Change surrounding' },
+			{ 'ds', desc = 'Delete surrounding' },
+		}
+	}
+}
