18d1daa8d504c21cf6bb3a3b763daa08837ce1ed
Samuel Jackson <dsiq3g@gmail.com>
2024-09-08 17:24:08 -0700

    Add indentation guide

---
+++ lua/packages/indentation_guide.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {
+			enabled = true,
+		},
+		keys = {
+			{
+				'<leader>ti',
+				function()
+					require("ibl").setup_buffer(0, {
+						enabled = not require("ibl.config").get_config(0).enabled,
+					})
+				end,
+				desc = 'Toggle indentation'
+			},
+		}
+	},
+}
