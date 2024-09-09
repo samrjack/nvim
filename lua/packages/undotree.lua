596cd012b40586e23a44010c3f4b968671f8974e
Samuel Jackson <dsiq3g@gmail.com>
2024-09-09 14:48:30 -0700

    Improve undotree experience

--- lua/packages/undotree.lua
+++ lua/packages/undotree.lua
@@ -3,8 +3,11 @@ return {
 	lazy = true,
 	config = function()
 		vim.g.undotree_UndoDir = vim.opt.undodir:get()
+		vim.g.undotree_undotree_WindowLayout = 2 -- style 2 has amuch bigger section for seeing the diff
+		vim.g.undotree_ShortIndicators = 1 -- Abbreviate times like 'day' into 'd'
+		vim.g.undotree_SetFocusWhenToggle = 1
 	end,
 	keys = {
-		{'<leader>u', vim.cmd.UndotreeToggle, desc = 'undo tree'},
+		{ '<leader>u', vim.cmd.UndotreeToggle, desc = 'undo tree' },
 	},
 }
