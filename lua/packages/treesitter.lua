55c6733e7f81527d209a528b9f65e85ad93bb147
Samuel Jackson <dsiq3g@gmail.com>
2024-09-09 14:47:57 -0700

    Add some treesitter text objects

--- lua/packages/treesitter.lua
+++ lua/packages/treesitter.lua
@@ -32,5 +32,29 @@ return {
 			'nvim-treesitter/nvim-treesitter',
 		},
 		event = { 'BufReadPre', 'BufNewFile' },
+		-- For future text objects, check https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+		config = function()
+			local opts = {
+				textobjects = {
+					select = {
+						enable = true,
+						keymaps = {
+							['if'] = { query = '@function.inner', desc = 'Inner function' },
+						}
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							['<leader>c>'] = '@parameter.inner',
+						},
+						swap_previous = {
+							['<leader>c<'] = '@parameter.inner',
+						},
+					},
+				},
+			}
+
+			require('nvim-treesitter.configs').setup(opts)
+		end
 	},
 }
