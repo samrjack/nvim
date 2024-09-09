4d4315ff4eab50de96123acc0b13eacb756da72b
Samuel Jackson <dsiq3g@gmail.com>
2024-09-08 17:22:23 -0700

    Formatting

--- lua/packages/git.lua
+++ lua/packages/git.lua
@@ -5,14 +5,18 @@ return {
 		'tpope/vim-fugitive',
 		lazy = true,
 		keys = {
-			{"<leader>gG", vim.cmd.Git, desc = "futitive"},
-			{'<leader>gB', function()
-				if  vim.bo.filetype == 'fugitiveblame' then
-					vim.api.nvim_win_close(0, true)
-				else
-					vim.cmd.Git('blame')
-				end
-			end, desc = 'toggle sideline blame'},
+			{ "<leader>gG", vim.cmd.Git, desc = "futitive" },
+			{
+				'<leader>gB',
+				function()
+					if vim.bo.filetype == 'fugitiveblame' then
+						vim.api.nvim_win_close(0, true)
+					else
+						vim.cmd.Git('blame')
+					end
+				end,
+				desc = 'toggle sideline blame'
+			},
 		},
 	},
 	{
@@ -28,20 +32,20 @@ return {
 		lazy = true,
 		config = true,
 		keys = {
-			{'<leader>gg', function() require('neogit').open() end, desc = 'Git status'},
+			{ '<leader>gg', function() require('neogit').open() end, desc = 'Git status' },
 		}
 	},
 	{
 		"f-person/git-blame.nvim",
 		lazy = true,
 		opts = {
-			enabled = false,  -- if you want to enable the plugin
+			enabled = false, -- if you want to enable the plugin
 			message_template = "  <date> • <summary> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
 			date_format = "%Y-%m-%d %H:%M:%S", -- template for the date, check Date format section for more options
-			virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+			virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
 		},
 		keys = {
-			{'<leader>gb', function() require('gitblame').toggle() end, desc = 'Toggle inline blame'},
+			{ '<leader>gb', function() require('gitblame').toggle() end, desc = 'Toggle inline blame' },
 		},
 	},
 }
--- lua/ui.lua
+++ lua/ui.lua
@@ -1,12 +1,12 @@
 -- Options that change the basic underlying vim UI settings
 
-vim.opt.number = true -- Turn on line numbers
-vim.opt.smartindent = true -- Automatically indent new lines
+vim.opt.number = true        -- Turn on line numbers
+vim.opt.smartindent = true   -- Automatically indent new lines
 vim.opt.termguicolors = true -- Enable 24-bit RGB colors
 
 -- indentation
-vim.opt.tabstop = 4 -- Number of spaces a tab represents
-vim.opt.shiftwidth = 4 -- Number of visual spces per tab
+vim.opt.tabstop = 4     -- Number of spaces a tab represents
+vim.opt.shiftwidth = 4  -- Number of visual spces per tab
 vim.opt.softtabstop = 4 -- number of spaces in a tab when editing
 
 -- searching
