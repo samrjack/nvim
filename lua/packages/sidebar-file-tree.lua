de9050e872aefa84f09592d80bc8e23e1a0c09ea
Samuel Jackson <dsiq3g@gmail.com>
2024-09-08 17:22:51 -0700

    Improve neotree behavior and add floating window keybinding

--- lua/packages/sidebar-file-tree.lua
+++ lua/packages/sidebar-file-tree.lua
@@ -8,9 +8,28 @@ return {
 			'munifTanjim/nui.nvim',
 			'3rd/image.nvim',
 		},
+		opts = {
+			add_blank_line_at_top = true,
+			close_if_last_window = true,
+			source_selector = {
+				winbar = true, -- Have clickable tabs for showing which mode neotree is in
+			},
+			window = {
+				mappings = {
+					['^'] = { 'navigate_up' },
+					['P'] = { 'toggle_preview', config = { use_float = true, use_image_nvim = true } },
+					['<Tab>'] = { 'toggle_node' },
+				},
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+			},
+		},
 		lazy = true,
 		keys = {
-			{ '<C-n>', function() vim.cmd.Neotree('toggle') end, desc = 'File tree' },
+			{ '<C-n>', ':Neotree left reveal toggle<cr>',  desc = 'File tree' },
+			{ 'g.',    ':Neotree float reveal toggle<cr>', desc = 'File tree' },
 		}
 	}
 }
