ac226f4b967126ef00e750f67bc07b3e77dd2e90
Samuel Jackson <dsiq3g@gmail.com>
2024-09-09 14:46:04 -0700

    Personalize the status line

--- lua/packages/status_line.lua
+++ lua/packages/status_line.lua
@@ -1,18 +1,63 @@
+local function neocodeium_status()
+	-- Tables to map serverstatus and status to corresponding symbols
+	local server_status_symbols = {
+		[0] = "󰣺", -- Connected
+		[1] = "󱤚", -- Connecting
+		[2] = "󰣽", -- Disconnected
+	}
+
+	local status_symbols = {
+		[0] = "󰚩", -- Enabled
+		[1] = "󱚧", -- Disabled Globally
+		[3] = "󱚢", -- Disabled for Buffer filetype
+		[5] = "󱚠", -- Disabled for Buffer encoding
+		[2] = "󱙻", -- Disabled for Buffer (catch-all)
+	}
+
+	local function connect(a, b)
+		return a .. " | " .. b
+	end
+
+	if not require("lazy.core.config").plugins["neocodeium"]._.loaded then
+		return connect('󰣼', status_symbols[2])
+	end
+	local status, serverstatus = require("neocodeium").get_status()
+
+	-- Handle serverstatus and status fallback (safeguard against any unexpected value)
+	local luacodeium = server_status_symbols[serverstatus] or "󰣼"
+	luacodeium = connect(luacodeium, (status_symbols[status] or status_symbols[2]))
+
+	return luacodeium
+end
+
 return {
 	{
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
 		opts = {
-		}
-	},
-	{
-		'kdheepak/tabline.nvim',
-		dependencies = {
-			'nvim-tree/nvim-web-devicons',
-			'nvim-lualine/lualine.nvim',
-		},
-		opts = {
-			enable = true,
+			sections = {
+				lualine_a = { 'mode' },
+				lualine_b = { 'branch', 'diff', 'diagnostics' },
+				lualine_c = { { 'filename', path = 3 } },
+				lualine_x = { 'encoding', 'fileformat', 'filetype', neocodeium_status },
+				lualine_y = { 'progress' },
+				lualine_z = { 'location' }
+			},
+			tabline = {
+				lualine_a = { 'buffers' },
+				lualine_b = { '' },
+				lualine_c = { 'filesize', 'filename' },
+				lualine_x = { 'searchcount', 'selectioncount' },
+				lualine_y = { { 'datetime', style = 'iso' } },
+				lualine_z = { 'tabs' }
+			},
+			extensions = {
+				'fugitive',
+				'lazy',
+				'nvim-dap-ui',
+				'nvim-tree',
+				'oil',
+			},
 		},
 	},
 }
