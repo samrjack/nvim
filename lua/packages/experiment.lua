a804743473b8068c3fd701d6b81005cd85d286a7
Samuel Jackson <dsiq3g@gmail.com>
2024-09-09 21:33:09 -0700

    Add some packages I want to try using in the future

---
+++ lua/packages/experiment.lua
@@ -0,0 +1,28 @@
+-- This Module is for all the packages I want to experiemnt with in the future
+-- but haven't fully configured yet or don't know if I want to keep them.
+
+return {
+	{
+		'samharju/yeet.nvim',
+		dependencies = {
+			'stevearc/dressing.nvim', -- optional, provides sane UX
+		},
+		lazy = true,
+		version = '*', -- use the latest release, remove for master
+		cmd = 'Yeet',
+		opts = {},
+	},
+	{
+		'rmagatti/goto-preview',
+		lazy = true,
+		opts = {},
+		keys = {
+			{ 'gpd', function() require('goto-preview').goto_preview_definition() end,      desc = 'Preview definition' },
+			{ 'gpt', function() require('goto-preview').goto_preview_type_definition() end, desc = 'Preview type definition' },
+			{ 'gpi', function() require('goto-preview').goto_preview_implementation() end,  desc = 'Preview implementation' },
+			{ 'gpD', function() require('goto-preview').goto_preview_declaration() end,     desc = 'Preview declarations' },
+			{ 'gP',  function() require('goto-preview').close_all_win() end,                desc = 'Close preivews' },
+			{ 'gpr', function() require('goto-preview').goto_preview_references() end,      desc = 'Preview references' },
+		},
+	},
+}
