db7a3b41499389fcfd23d44feb78a52a2e912f29
Samuel Jackson <dsiq3g@gmail.com>
2024-09-12 12:42:40 -0700

    Format dates package

--- lua/packages/dates.lua
+++ lua/packages/dates.lua
@@ -3,10 +3,10 @@ return {
 		'https://github.com/tpope/vim-speeddating',
 		lazy = true,
 		keys = {
-			{'<C-a>', mode = {'n', 'v'}, desc = 'Increment number/date'},
-			{'<C-x>', mode = {'n', 'v'}, desc = 'Decrement number/date'},
-			{'d<C-a>', mode = {'n', 'v'}, desc = 'Set date UTC'},
-			{'d<C-x>', mode = {'n', 'v'}, desc = 'Set date local'},
+			{ '<C-a>',  mode = { 'n', 'v' }, desc = 'Increment number/date' },
+			{ '<C-x>',  mode = { 'n', 'v' }, desc = 'Decrement number/date' },
+			{ 'd<C-a>', mode = 'n',          desc = 'Set date UTC' },
+			{ 'd<C-x>', mode = 'n',          desc = 'Set date local' },
 		},
 	},
 	{
