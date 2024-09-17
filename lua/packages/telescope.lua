52cd2b0975e79e067e972713e5aca6669b8258f0
Samuel Jackson <dsiq3g@gmail.com>
2024-09-16 21:50:37 -0700

    Add better telescope configurations

--- lua/packages/telescope.lua
+++ lua/packages/telescope.lua
@@ -4,11 +4,33 @@ return {
 		'nvim-lua/plenary.nvim',
 	},
 	lazy = true,
+	opts = {
+		defaults = {
+			mappings = {
+				i = {
+					['<C-h>'] = 'which_key',
+				},
+				n = {
+					['q'] = 'close',
+				}
+			}
+		},
+	},
 	keys = {
-		{'<leader>ff', function() require('telescope.builtin').find_files() end, desc = 'find files'},
-		{'<leader>fp', function() require('telescope.builtin').git_files() end, desc = 'project files'},
-		{'<leader>fg', function() require('telescope.builtin').live_grep() end, desc = 'grep files'},
-		{'<leader>fb', function() require('telescope.builtin').buffers() end, desc = 'find buffer'},
-		{'<leader>fh', function() require('telescope.builtin').help_tags() end, desc = 'find help tag'},
+		{ '<leader>fa', function() require('telescope.builtin').builtin() end,         desc = 'All pickers' },
+		{ '<leader>fb', function() require('telescope.builtin').buffers() end,         desc = 'Buffer' },
+		{ '<leader>ff', function() require('telescope.builtin').find_files() end,      desc = 'Files' },
+		{ '<leader>fg', function() require('telescope.builtin').live_grep() end,       desc = 'Grep files' },
+		{ '<leader>fh', function() require('telescope.builtin').help_tags() end,       desc = 'Help tag' },
+		{ '<leader>fm', function() require('telescope.builtin').man_pages() end,       desc = 'Man page' },
+		{ '<leader>fM', function() require('telescope.builtin').marks() end,           desc = 'Marks' },
+		{ '<leader>fp', function() require('telescope.builtin').git_files() end,       desc = 'Project files' },
+		{ '<leader>ft', function() require('telescope.builtin').treesitter() end,      desc = 'Treesitter' },
+		{ '<leader>ha', function() require('telescope.builtin').autocommands() end,    desc = 'Describe variable' },
+		{ '<leader>hc', function() require('telescope.builtin').colorscheme() end,     desc = 'Colorschemes' },
+		{ '<leader>hf', function() require('telescope.builtin').filetypes() end,       desc = 'Change filetype' },
+		{ '<leader>hv', function() require('telescope.builtin').vim_options() end,     desc = 'Describe variable' },
+		{ '<leader>sc', function() require('telescope.builtin').command_history() end, desc = 'Command history' },
+		{ '<leader>ss', function() require('telescope.builtin').search_history() end,  desc = 'Search history' },
 	},
 }
