881cf625f2ffffcce867f7427f74833712ca88fd
Samuel Jackson <dsiq3g@gmail.com>
2024-09-16 21:52:02 -0700

    Format insert movements

--- lua/keys/general/insert-movements.lua
+++ lua/keys/general/insert-movements.lua
@@ -1,14 +1,13 @@
-
 -- check :h map-listing for info on effected modes
 
 -- Move by character amounts
-vim.keymap.set({'!','t'}, '<C-h>', '<Left>', {desc = "Move one character left"})
-vim.keymap.set({'!','t'}, '<C-j>', '<Down>', {desc = "Move one character down"})
-vim.keymap.set({'!','t'}, '<C-k>', '<Up>', {desc = "Move one character up"})
-vim.keymap.set({'!','t'}, '<C-l>', '<Right>', {desc = "Move one character right"})
+vim.keymap.set({ '!', 't' }, '<C-h>', '<Left>', { desc = "Move one character left" })
+vim.keymap.set({ '!', 't' }, '<C-j>', '<Down>', { desc = "Move one character down" })
+vim.keymap.set({ '!', 't' }, '<C-k>', '<Up>', { desc = "Move one character up" })
+vim.keymap.set({ '!', 't' }, '<C-l>', '<Right>', { desc = "Move one character right" })
 
 -- Move by word amounts
-vim.keymap.set({'!','t'}, '<M-h>', '<C-Left>', {desc = "Move one word left"})
-vim.keymap.set({'!','t'}, '<M-j>', '<C-o>}', {desc = "Move one paragraph down"})
-vim.keymap.set({'!','t'}, '<M-k>', '<C-o>{', {desc = "Move one paragraph up"})
-vim.keymap.set({'!','t'}, '<M-l>', '<C-Right>', {desc = "Move one word right"})
+vim.keymap.set({ '!', 't' }, '<M-h>', '<C-Left>', { desc = "Move one word left" })
+vim.keymap.set({ '!', 't' }, '<M-j>', '<C-o>}', { desc = "Move one paragraph down" })
+vim.keymap.set({ '!', 't' }, '<M-k>', '<C-o>{', { desc = "Move one paragraph up" })
+vim.keymap.set({ '!', 't' }, '<M-l>', '<C-Right>', { desc = "Move one word right" })
