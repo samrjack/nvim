879921e63feed513dd515d95c98d63eed6e931fc
Samuel Jackson <dsiq3g@gmail.com>
2024-09-27 11:09:10 -0700

    Add a games file
    For now just adding a game for practicing motions

---
+++ lua/packages/games.lua
@@ -0,0 +1,8 @@
+return {
+	-- This is a collection of games meant for practicing vim motions and becoming more effective
+	{
+		'ThePrimeagen/vim-be-good',
+		lazy = true,
+		cmd = 'VimBeGood'
+	}
+}
