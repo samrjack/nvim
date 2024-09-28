dbc878ed35f0cc78a2211d0be3768dfcae48939d
Samuel Jackson <dsiq3g@gmail.com>
2024-09-27 21:46:31 -0700

    Turn on vim spelling

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
-require('keys')
-require('ui')
-require('scratch')
 require('files')
 require('folding')
+require('keys')
+require('scratch')
+require('spelling')
+require('ui')
 
 -- Comes last so all other general configuration can be completed and built upon in the plugins
 require('lazy_setup')
