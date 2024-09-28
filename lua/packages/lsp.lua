2bab02fbf41febe4a4b2b39641416cce41eedf4c
Samuel Jackson <dsiq3g@gmail.com>
2024-09-27 22:05:59 -0700

    Update completion engine to look better and work better
    Also added a snippets package for pre-made snippets.

--- lua/packages/lsp.lua
+++ lua/packages/lsp.lua
@@ -6,18 +6,36 @@ return {
 		lazy = true,
 	},
 	{
+		'rafamadriz/friendly-snippets',
+		lazy = true,
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
+	{
 		'L3MON4D3/LuaSnip',
+		dependencies = {
+			'rafamadriz/friendly-snippets',
+		},
 		-- follow latest release.
 		version = 'v2.*', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 		-- install jsregexp (optional!).
 		build = 'make install_jsregexp',
 		lazy = true
+
 	},
 	{
 		'hrsh7th/nvim-cmp',
 		dependencies = {
 			'L3MON4D3/LuaSnip',
 			'saadparwaiz1/cmp_luasnip',
+			'hrsh7th/cmp-nvim-lsp', -- Complete from lsp suggestions
+			'hrsh7th/cmp-nvim-lua', -- Complete nvim lua api
+			'hrsh7th/cmp-buffer', -- Complete words and items from the buffer
+			'hrsh7th/cmp-path', -- Complete file name
+			'hrsh7th/cmp-cmdline', -- Completion for Vim's commandline
+			'onsails/lspkind.nvim', -- Clean up recommendation box and show source
+			'f3fora/cmp-spell', -- Spelling recommendations
 		},
 		lazy = true,
 		opts = function() -- When opts is a function, it needs to return the table to be used for setup
@@ -30,21 +48,86 @@ return {
 						require('luasnip').lsp_expand(args.body)
 					end
 				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
 				mapping = cmp.mapping.preset.insert({
 					['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 					['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-					['<CR>'] = cmp.mapping.confirm({ select = true }),
-					['<C-y>'] = cmp.mapping.confirm({ select = true }),
+					['<C-y>'] = cmp.mapping({
+						i = function(fallback)
+							if cmp.visible() and cmp.get_active_entry() then
+								cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+							else
+								fallback()
+							end
+						end,
+						c = cmp.mapping.confirm({ select = true })
+					}),
+					['<CR>'] = cmp.mapping({
+						i = function(fallback)
+							if cmp.visible() and cmp.get_active_entry() then
+								cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+							else
+								fallback()
+							end
+						end,
+						s = cmp.mapping.confirm({ select = true }),
+						c = function(fallback)
+							if cmp.visible() and cmp.get_active_entry() then
+								cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+							else
+								fallback()
+							end
+						end,
+					}),
 					['<C-Space>'] = cmp.mapping.complete(),
 				}),
 				sources = cmp.config.sources({
-						{ name = 'nvim_lsp' },
-						{ name = 'luasnip' },
-					},
-					{
-						{ name = 'buffer' },
-					}),
+					{ name = 'nvim_lsp' },
+					{ name = 'nvim_lua' },
+					{ name = 'luasnip' },
+					{ name = 'buffer' },
+				}, {
+					{ name = 'path' },
+				}, {
+					{ name = 'spell' },
+				}),
+				formatting = {
+					format = require('lspkind').cmp_format({
+						with_text = true,
+						show_labelDetails = true,
+						menu = {
+							buffer = '[buf]',
+							nvim_lsp = '[LSP]',
+							nvim_lua = '[api]',
+							path = '[path]',
+							luasnip = '[snip]',
+							spell = '[spell]',
+						}
+					})
+				},
+				experimental = {
+					ghost_text = true, -- show recommendation as typing takes place
+				},
 			}
+		end,
+		config = function(_, opts)
+			local cmp = require('cmp')
+
+			cmp.setup(opts)
+
+			cmp.setup.cmdline({ '/', '?' }, {
+				mapping = cmp.mapping.preset.cmdline()
+			})
+			cmp.setup.cmdline({ ':', {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = 'path' },
+					{ name = 'cmdline' },
+				})
+			} })
 		end,
 	},
 	{
@@ -100,10 +183,6 @@ return {
 		dependencies = {
 			'williamboman/mason.nvim',
 			'williamboman/mason-lspconfig.nvim',
-			'hrsh7th/cmp-nvim-lsp',
-			'hrsh7th/cmp-buffer',
-			'hrsh7th/cmp-path',
-			'hrsh7th/cmp-cmdline',
 			'hrsh7th/nvim-cmp',
 		},
 		lazy = true,
