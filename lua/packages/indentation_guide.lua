6d82b84c37bafb4f99782db88ef0f533f9eccb89
Samuel Jackson <dsiq3g@gmail.com>
2024-10-18 08:59:58 -0700

    Configure indentation guides

--- lua/packages/indentation_guide.lua
+++ lua/packages/indentation_guide.lua
@@ -1,21 +1,73 @@
+local function toggleHLChunk()
+	if vim.g.hlchunkDisabled then
+		-- vim.cmd.EnableHLBlank()
+		vim.cmd.EnableHLIndent()
+		vim.cmd.EnableHLChunk()
+		vim.cmd.EnableHLLineNum()
+	else
+		-- vim.cmd.DisableHLBlank()
+		vim.cmd.DisableHLIndent()
+		vim.cmd.DisableHLChunk()
+		vim.cmd.DisableHLLineNum()
+	end
+	vim.g.hlchunkDisabled = not vim.g.hlchunkDisabled
+end
+
 return {
 	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		opts = {
-			enabled = true,
-		},
+		"shellRaining/hlchunk.nvim",
 		lazy = true,
+		opts = {
+			chunk = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+				use_treesitter = true,
+				chars = {
+					"│",
+				},
+				filter_list = {
+					function(v)
+						return v.level ~= 1
+					end,
+				},
+			},
+			blank = {
+				enable = false,
+			},
+			line_num = {
+				enable = true,
+			},
+		},
+		config = function(_, opts)
+			require('hlchunk').setup(opts)
+			toggleHLChunk()
+		end,
 		keys = {
 			{
-				'<leader>ti',
-				function()
-					require("ibl").setup_buffer(0, {
-						enabled = not require("ibl.config").get_config(0).enabled,
-					})
-				end,
-				desc = 'Toggle indentation'
-			},
+				'<leader>ti', function() toggleHLChunk() end, desc = 'Toggle Indentation'
+			}
 		}
 	},
+	-- Doesn't seem to work as desired
+	-- {
+	-- 	"lukas-reineke/indent-blankline.nvim",
+	-- 	main = "ibl",
+	-- 	opts = {
+	-- 		enabled = true,
+	-- 	},
+	-- 	lazy = true,
+	-- 	keys = {
+	-- 		{
+	-- 			'<leader>ti',
+	-- 			function()
+	-- 				require("ibl").setup_buffer(0, {
+	-- 					enabled = not require("ibl.config").get_config(0).enabled,
+	-- 				})
+	-- 			end,
+	-- 			desc = 'Toggle indentation'
+	-- 		},
+	-- 	}
+	-- },
 }
